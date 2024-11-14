d70e325cce2fa0a3aa883a16e0dce20b01119fd2
Emanuele Bianchi <emanuelebianchi00@gmail.com>
2024-11-14 16:58:41 +0100

    Update

--- script.js
+++ script.js
@@ -20,10 +20,39 @@ const getimage = function (url) {
     })
     .then((images) => {
       //   console.log(images);
-      const cards = document.querySelectorAll(".card");
-
       images.forEach((img) => {
-        cards.forEach((card) => {});
+        console.log(img);
+        const row = document.querySelector(".row");
+        const column = document.createElement("div");
+        column.className = "col-md-4";
+        const card = document.createElement("div");
+        card.className = "card mb-4 shadow-sm";
+        const cardImg = document.createElement("img");
+        // cardImg.src = img.
+
+        // `<div class="col-md-4">
+        //     <div class="card mb-4 shadow-sm">
+        //       <img src="https://picsum.photos/id/237/300/200" class="bd-placeholder-img card-img-top" />
+        //       <div class="card-body">
+        //         <h5 class="card-title">Lorem Ipsum</h5>
+        //         <p class="card-text">
+        //           This is a wider card with supporting text below as a natural
+        //           lead-in to additional content. This content is a little bit
+        //           longer.
+        //         </p>
+        //         <div class="d-flex justify-content-between align-items-center">
+        //           <div class="btn-group">
+        //             <button type="button" class="btn btn-sm btn-outline-secondary">
+        //               View
+        //             </button>
+        //             <button type="button" class="btn btn-sm btn-outline-secondary">
+        //               Edit
+        //             </button>
+        //           </div>
+        //           <small class="text-muted">9 mins</small>
+        //         </div>
+        //       </div>
+        //     </div>`;
       });
       //   Recuperare le card
       //   Inserire al posto dell'src delle img l'immagine presa dall'url con la GET.
